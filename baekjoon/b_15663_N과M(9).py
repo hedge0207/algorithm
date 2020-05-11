343e2a733b566f9b5ff03e3647881fa6e503b43b
hedge0913 <hedge0207@gmail.com>
2020-05-11 23:55:17 +0900

    0511

--- baekjoon/b_15663_N과M(9).py
+++ baekjoon/b_15663_N과M(9).py
@@ -1,27 +1,28 @@
-#미완
-def f(k):
-    global t
+#아직도 미완
+def f(k,c):
     if k==M:
         for i in l:
             print(i,end=" ")
         print()
     else:
         for i in range(len(arr)):
-            if visited[i]:
-                continue
-            else:
+            if visited[i] ==0 and arr[i] not in vl[k]:
+                vl[k].append(arr[i])
                 visited[i]=1
                 l.append(arr[i])
-                f(k+1)
-                visited[i]=0
+                f(k+1,i)
                 l.pop()
+
+
 
 N, M = map(int,input().split())
 arr = list(map(int,input().split()))
-visited= [0]*(N+1)
+visited = [0]*N
+vl = [[] for _ in range(M)]
+print(visited)
 arr.sort()
+
 t = []
-
 l=[]
-f(0)
-
+f(0,"C")
+print(vl)
