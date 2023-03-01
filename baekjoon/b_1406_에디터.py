158dc70679df45a59362941de45233a2f3dcc9d9
hedge0207 <hedge0207@gmail.com>
2023-03-01 21:31:30 +0900

    230301

--- baekjoon/b_1406_에디터.py
+++ baekjoon/b_1406_에디터.py
@@ -1,104 +1,78 @@
 import sys
 
-
 input = sys.stdin.readline
 
+
 class Node:
-    def __init__(self, char, pre=None, pos=None):
+    def __init__(self, char, prev=None, post=None):
         self.char = char
-        self.pre = pre
-        self.pos = pos
+        self.prev = prev
+        self.post = post
 
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        init_node = Node(None)
+        self.head = init_node
         self.tail = None
-        self.size = 0
+        self.cur = self.tail
+
+    def insert_node(self, char):
+        node = Node(char)
+        if self.cur == self.tail:
+            if self.tail is not None:
+                node.prev = self.tail
+                self.tail.post = node
+            else:
+                node.prev = self.head
+                self.head.post = node
+            self.tail = node
+        elif self.cur == None:
+            node.post = self.head.post
+            node.prev = self.head
+            self.head.post = node
+            self.head.post.prev = node
+        else:
+            node.post = self.cur.post
+            node.prev = self.cur
+            self.cur.post.prev = node
+            self.cur.post = node
+        self.cur = node
+
+    def remove_node(self):
+        self.cur.prev.post = self.cur.post
+        if self.cur.post is not None:
+            self.cur.post.prev = self.cur.prev
+        else:
+            self.tail = self.cur.prev
+        self.cur = self.cur.prev
 
     def print_list(self):
-        cur = linked_list.head.pos
+        cur = self.head.post
         while cur:
             print(cur.char, end="")
-            cur = cur.pos
-        print()
-        cur = linked_list.tail
-        while 1:
-            print(cur.char, end="")
-            cur = cur.pre
-            if cur.char=="H":
-                break
-        print()
-        print("-"*100)
-
-
-    def link_nodes(self, elements):
-        for i in range(len(elements)):
-            node = Node(text[i])
-            if linked_list.tail is None:
-                linked_list.head = node
-            else:
-                node.pre = linked_list.tail
-                linked_list.tail.pos = node
-            linked_list.tail = node
-            self.size += 1
-
-    def add_head(self):
-        cur_head = self.head
-        new_head = Node("H", None, cur_head)
-        self.head = new_head
-        cur_head.pre = new_head
-
+            cur = cur.post
 
 
 text = input().rstrip()
-linked_list = LinkedList()
-linked_list.link_nodes(text)
-linked_list.add_head()
+N = int(input())
+ll = LinkedList()
 
-n = int(input())
-commands = [input().rstrip() for _ in range(n)]
+for char in text:
+    ll.insert_node(char)
 
-cur = linked_list.tail
-for command in commands:
-    print(command)
-    is_tail = False
-    if cur == linked_list.tail:
-        is_tail = True
-
-    is_head = False
-    if cur == linked_list.head:
-        is_head = True
-
+for i in range(N):
+    command = input().rstrip()
     if command == "L":
-        if is_head:
-            continue
-        cur = cur.pre
+        if ll.cur.char is not None:
+            ll.cur = ll.cur.prev
     elif command == "D":
-        if is_tail:
-            continue
-        cur = cur.pos
+        if ll.cur.post is not None:
+            ll.cur = ll.cur.post
     elif command == "B":
-        if is_head:
-            continue
-        cur.pre.pos = cur.pos
-        cur.pos.pre = cur.pre
-        cur = cur.pre
-        linked_list.size -= 1
+        if ll.cur.char is not None:
+            ll.remove_node()
     else:
-        print(cur.char)
-        char = command.split()[1]
-        node = Node(char, cur, cur.pos)
-        cur.pos = node
-        cur = node
-        print(cur.char)
-        print(cur.pre.char)
-        if is_tail:
-            linked_list.tail = node
+        ll.insert_node(command[2])
 
-        if is_head:
-            linked_list.head.pos.pre = node
-            linked_list.head.pos = node
-            node.pre = linked_list.head
-        linked_list.size += 1
-    linked_list.print_list()
+ll.print_list()
