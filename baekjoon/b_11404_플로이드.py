69c455c1b4cc1e109a3b570b39cf72d3365e2f5d
hedge0207 <hedge0207@gmail.com>
2023-03-25 14:12:21 +0900

    add Floyd algorithm

--- baekjoon/b_11404_플로이드.py
+++ baekjoon/b_11404_플로이드.py
@@ -6,7 +6,6 @@ N = int(input())
 M = int(input())
 MAX_COST = float("infinity")
 graph = [[MAX_COST] * (N + 1) for _ in range(N + 1)]
-
 for _ in range(M):
     st, ed, cost = map(int, input().split())
     graph[st][ed] = min(cost, graph[st][ed])
@@ -28,28 +27,3 @@ for i in range(1, N + 1):
             graph[i][j] = 0
         print(graph[i][j], end=" ")
     print()
-
-
-"""
-input
-순서대로 vertex의 개수 N
-간선의 개수 M
-3번째 줄 부터 M+2번 줄까지 출발 정점, 도착 정점, 가중치
-
-5
-14
-1 2 2
-1 3 3
-1 4 1
-1 5 10
-2 4 2
-3 4 1
-3 5 1
-4 5 3
-3 5 10
-3 1 8
-1 4 2
-5 1 7
-3 4 2
-5 2 4
-"""
