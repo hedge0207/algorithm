a3c113af8259efed32a23b536fb1158fd9ea444a
hedge0207 <hedge0207@gmail.com>
2023-07-15 16:11:57 +0900

    230715

---
+++ leetcode/509_Fibonacci Number.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+class Tabulation:
+    dp = defaultdict(int)
+
+    def fib(self, n: int) -> int:
+        self.dp[0] = 0
+        self.dp[1] = 1
+
+        for i in range(2, n + 1):
+            self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
+        return self.dp[n]
+
+
+class Memoization:
+    dp = defaultdict(int)
+
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+
+        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.dp[n]
