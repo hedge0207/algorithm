c675ef9d867d5601b23e1072d60261275a2ab251
hedge0207 <hedge0207@gmail.com>
2026-09-13 10:52:10 +0900

    260913

---
+++ leetcode/1081_Smallest_Subsequence_of_Distinct_Characters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        indices_per_char = {}
+        for i in range(len(s)):
+            char = s[i]
+            if char in indices_per_char:
+                indices_per_char[char].add(i)
+            else:
+                indices_per_char[char] = {i}
+
+        ans = []
+        for i in range(len(s)):
+            char = s[i]
+            indices_per_char[char].remove(i)
+            if char in ans:
+                continue
+            while ans:
+                post_char = ans[-1]
+                if ord(post_char) > ord(char) and indices_per_char[post_char]:
+                    ans.pop()
+                else:
+                    break
+            ans.append(char)
+        return "".join(ans)
